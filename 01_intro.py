5f4114e95a18c6d4b5c989e8d155b468ad01a9b9
diseo <diseo_rvt@hotmail.es>
2024-03-08 23:24:14 -0600

    Se agrega la grafica de pay

--- 01_intro.py
+++ 01_intro.py
@@ -1,7 +1,13 @@
 import matplotlib.pyplot as plt
 
-# Create a simple plot
-plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+def generar_pie():
+    etiquetas = ['A', 'B', 'C']
+    valores = [200, 34, 120]
 
-# Show the plot
-plt.show()
+    fig, ax = plt.subplots()
+    ax.pie(valores, labels=etiquetas, autopct='%1.1f%%')
+
+    plt.savefig('grafico_pie.png')
+    plt.close()
+
+generar_pie()
